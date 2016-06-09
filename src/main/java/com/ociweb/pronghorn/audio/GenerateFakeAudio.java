32de2643bd5235fa0f23ead7622249bc8ee588e7
Nathan Tippy <nathan@tippy.name>
2016-06-09 14:58:38 -0500

    quick mic demo

--- src/main/java/com/ociweb/pronghorn/audio/GenerateFakeAudio.java
+++ src/main/java/com/ociweb/pronghorn/audio/GenerateFakeAudio.java
@@ -1,5 +1,13 @@
 package com.ociweb.pronghorn.audio;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioFormat.Encoding;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Port;
+import javax.sound.sampled.TargetDataLine;
+
 import com.ociweb.pronghorn.pipe.Pipe;
 import com.ociweb.pronghorn.pipe.PipeWriter;
 import com.ociweb.pronghorn.stage.PronghornStage;
@@ -8,6 +16,7 @@ import com.ociweb.pronghorn.stage.scheduling.GraphManager;
 public class GenerateFakeAudio extends PronghornStage {
 
     private Pipe<AudioSchema> output;
+    private TargetDataLine line;
     
     protected GenerateFakeAudio(GraphManager graphManager, Pipe<AudioSchema> output) {
         super(graphManager, NONE, output);
@@ -17,6 +26,27 @@ public class GenerateFakeAudio extends PronghornStage {
     @Override
     public void startup() {
         
+        float sampleRate = 0;
+        int sampleSizeInBits = 0;
+        int channels = 0;
+        int frameSize = 0;
+        float frameRate = 0;
+        boolean bigEndian = false;
+        AudioFormat af = new AudioFormat(Encoding.PCM_SIGNED, sampleRate, sampleSizeInBits, channels, frameSize, frameRate, bigEndian);
+        
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, af); // format is an AudioFormat object
+        if (!AudioSystem.isLineSupported(info)) {
+            // Handle the error ... 
+
+        }
+        // Obtain and open the line.
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(af);
+        } catch (LineUnavailableException ex) {
+            // Handle the error ... 
+        }
+        
     }
 
     @Override
@@ -25,6 +55,12 @@ public class GenerateFakeAudio extends PronghornStage {
         if (PipeWriter.tryWriteFragment(output, AudioSchema.MSG_SAMPLE_110)) {
             
             int value = (int)(1000*Math.random());
+            
+            //use line method to pull in the mic data.
+            //line.re
+            
+            
+            
             PipeWriter.writeInt(output, AudioSchema.MSG_SAMPLE_110_FIELD_VALUE_111, value);
             PipeWriter.publishWrites(output);
         }
